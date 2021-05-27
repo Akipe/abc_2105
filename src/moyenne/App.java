9c5e8a49ef992f0530bc800c380f52b4ca5c9676
Julien Milletre Akipe <code.julien@milletre.fr>
2021-05-27 14:37:01 +0200

    initial project

---
+++ src/moyenne/App.java
@@ -0,0 +1,31 @@
+package moyenne;
+
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+
+        System.out.println("Exercice 1.1.1 : Calcul de la moyenne de deux nombres");
+        
+        /**
+         * Algorithme
+         * 
+         * FONCTIONS_UTILISEES
+         * VARIABLES
+         *     premierNombre EST_DU_TYPE NOMBRE
+         *     deuxiemeNombre EST_DU_TYPE NOMBRE
+         * DEBUT_ALGORITHME
+         *     AFFICHER* "Bienvenu dans l'app pour calculer la moyenne de deux nombres"
+         *     AFFICHER* "Veuillez écrire votre premier nombre :"
+         *     LIRE premierNombre
+         *     AFFICHER* "Veuillez écrire votre deuxième nombre :"
+         *     LIRE deuxiemeNombre
+         *     AFFICHERCALCUL (premierNombre+deuxiemeNombre)/2
+         * FIN_ALGORITHME
+         * 
+         */
+        System.out.println("Veuillez saisir le premier nombre :");
+    }
+}
