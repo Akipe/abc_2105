ab1d43ac402c08ec07f12c6f5fd08666f1f57dfb
Julien Milletre Akipe <code.julien@milletre.fr>
2021-05-31 12:09:25 +0200

    ex 1.6 : rewrite vars

--- src/primenumbers/App.java
+++ src/primenumbers/App.java
@@ -10,65 +10,65 @@ public class App {
          * Algorithme
             FONCTIONS_UTILISEES
             VARIABLES
-                numberToTestPrimeNumber EST_DU_TYPE NOMBRE
-                numberForCheckPrimeNumber EST_DU_TYPE NOMBRE
+                numberToTest EST_DU_TYPE NOMBRE
+                numberForCheckDivision EST_DU_TYPE NOMBRE
                 isPrimeNumber EST_DU_TYPE NOMBRE
-                listDiviserFornumberToTestPrimeNumber EST_DU_TYPE CHAINE
+                listValidDiviser EST_DU_TYPE CHAINE
             DEBUT_ALGORITHME
                 AFFICHER* "Veuillez entrer un nombre pour vérifier s'il est premier :"
-                LIRE numberToTestPrimeNumber
-                numberForCheckPrimeNumber PREND_LA_VALEUR numberToTestPrimeNumber-1
+                LIRE numberToTest
+                numberForCheckDivision PREND_LA_VALEUR numberToTest-1
                 isPrimeNumber PREND_LA_VALEUR 1
-                TANT_QUE (numberForCheckPrimeNumber>1) FAIRE
+                TANT_QUE (numberForCheckDivision>1) FAIRE
                     DEBUT_TANT_QUE
-                    SI (numberToTestPrimeNumber%numberForCheckPrimeNumber==0) ALORS
+                    SI (numberToTest%numberForCheckDivision==0) ALORS
                         DEBUT_SI
-                        listDiviserFornumberToTestPrimeNumber PREND_LA_VALEUR listDiviserFornumberToTestPrimeNumber + " " + numberForCheckPrimeNumber
+                        listValidDiviser PREND_LA_VALEUR listValidDiviser + " " + numberForCheckDivision
                         isPrimeNumber PREND_LA_VALEUR 1
                         FIN_SI
-                    numberForCheckPrimeNumber PREND_LA_VALEUR numberForCheckPrimeNumber-1
+                    numberForCheckDivision PREND_LA_VALEUR numberForCheckDivision-1
                     FIN_TANT_QUE
                 SI (isPrimeNumber==1) ALORS
                     DEBUT_SI
                     AFFICHER "Le nombre "
-                    AFFICHER numberToTestPrimeNumber
+                    AFFICHER numberToTest
                     AFFICHER* " est un nombre premier."
                     FIN_SI
                     SINON
                         DEBUT_SINON
                         AFFICHER "Le nombre "
-                        AFFICHER numberToTestPrimeNumber
+                        AFFICHER numberToTest
                         AFFICHER " n'est pas un nombre premier, il est divisable par :"
-                        AFFICHER* listDiviserFornumberToTestPrimeNumber
+                        AFFICHER* listValidDiviser
                         FIN_SINON
             FIN_ALGORITHME
          */
 
 
-        int     numberToTestPrimeNumber      = 0,
-                numberForCheckPrimeNumber    = 0;
-        boolean isPrimeNumber               = true;
-        String  listDiviserFornumberToTestPrimeNumber = new String("");
-        Scanner scanner                     = new Scanner(System.in);
+        int     numberToTest            = 0,
+                numberForCheckDivision  = 0;
+        boolean isPrimeNumber           = true;
+        String  listValidDiviser        = new String("");
+        Scanner scanner                 = new Scanner(System.in);
 
 
         System.out.println("Veuillez entrer un nombre pour vérifier s'il est premier :");
-        numberToTestPrimeNumber = scanner.nextInt();
+        numberToTest = scanner.nextInt();
         scanner.close();
 
         // Verify by testing to divide all numbers from number himself-1 to 2,
         // If the number can be divided without rest, it is not a prime number.
-        for (numberForCheckPrimeNumber = numberToTestPrimeNumber - 1; numberForCheckPrimeNumber > 1; numberForCheckPrimeNumber--) {
-            if ((numberToTestPrimeNumber % numberForCheckPrimeNumber) == 0) {
-                listDiviserFornumberToTestPrimeNumber = listDiviserFornumberToTestPrimeNumber + " " + numberForCheckPrimeNumber;
+        for (numberForCheckDivision = numberToTest - 1; numberForCheckDivision > 1; numberForCheckDivision--) {
+            if ((numberToTest % numberForCheckDivision) == 0) {
+                listValidDiviser = listValidDiviser + " " + numberForCheckDivision;
                 isPrimeNumber = false;
             }
         }
 
         if (isPrimeNumber) {
-            System.out.println("Le nombre " + numberToTestPrimeNumber + " est un nombre premier.");
+            System.out.println("Le nombre " + numberToTest + " est un nombre premier.");
         } else {
-            System.out.println("Le nombre " + numberToTestPrimeNumber + " n'est pas un nombre premier, il est divisable par :" + listDiviserFornumberToTestPrimeNumber);
+            System.out.println("Le nombre " + numberToTest + " n'est pas un nombre premier, il est divisable par :" + listValidDiviser);
         }
     }
 }
