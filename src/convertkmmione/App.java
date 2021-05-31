2c3f543a95e894e64ed99ac3524e190e4a491844
Julien Milletre Akipe <code.julien@milletre.fr>
2021-05-31 16:56:13 +0200

    Add initial src for exercice 2.1.1 convert km mi

---
+++ src/convertkmmione/App.java
@@ -0,0 +1,13 @@
+package convertkmmione;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Exerice 2.1.1: Conversion kilomètres et Miles.");
+    
+        /**
+         * Algorithme
+         * 
+         * Open the file "exo_2.1.1_conversion_kilometres_miles.eap" with Enterprise Architect 7.1
+         */
+    }
+}
