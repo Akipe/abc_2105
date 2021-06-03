9477d91684ec82c6fac3a180eca954b3a238ca4d
Julien Milletre Akipe <code.julien@milletre.fr>
2021-06-03 13:01:29 +0200

    Add initial exercice 2.1.2

---
+++ src/convertkmmitwo/App.java
@@ -0,0 +1,14 @@
+package convertkmmitwo;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Exerice 2.1.1: Conversion kilomètres et Miles.");
+
+        /**
+         * 
+         * Open the file exo_2.1.2_conversion_kilometres_miles.png
+         * or exo_2.1.2_conversion_kilometres_miles.eap with Enterprise Architect 7.1
+         */
+    }
+}
