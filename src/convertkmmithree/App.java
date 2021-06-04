05a1c61815598ce8ef48c1057e782bd68433c164
Julien Milletre Akipe <code.julien@milletre.fr>
2021-06-04 08:36:01 +0200

    Add initial exercice 2.1.3

---
+++ src/convertkmmithree/App.java
@@ -0,0 +1,15 @@
+package convertkmmithree;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Exerice 2.1.3: Conversion kilomètres et Miles.");
+
+        /**
+         * 
+         * Open the file exo_2.1.3_conversion_kilometres_miles.png
+         * or exo_2.1.3_conversion_kilometres_miles.eap with Enterprise Architect 7.1
+         */
+    }
+}
