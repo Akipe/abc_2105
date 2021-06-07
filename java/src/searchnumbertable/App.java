af4e3b36b56fd12ace93a33785b0bfa1fc7d2493
Julien Milletre Akipe (asdHome) <code.julien@milletre.fr>
2021-06-07 12:18:59 +0200

    Add initial sources for exercice 3.1

---
+++ java/src/searchnumbertable/App.java
@@ -0,0 +1,15 @@
+package searchnumbertable;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Exercice 3.1: Recherche un nombre dans un tableau.");
+
+        /**
+         * 
+         * Open the file exo_3.1_recherche_nombre_tableau.png
+         * or exo_3.1_recherche_nombre_tableau.eap with Enterprise Architect 7.1
+         */
+    }
+}
