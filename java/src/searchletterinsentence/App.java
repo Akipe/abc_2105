812f649c4ccf5f80c77c235d09592c9be87ff140
Julien Milletre Akipe (asdHome) <code.julien@milletre.fr>
2021-06-08 09:45:49 +0200

    Exercice 3.2 add app logic with comments

--- java/src/searchletterinsentence/App.java
+++ java/src/searchletterinsentence/App.java
@@ -1,5 +1,7 @@
 package searchletterinsentence;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Exercice 3.2: Recherche une lettre dans une chaine de caractère.");
@@ -11,6 +13,39 @@ public class App {
          */
 
 
-        
+        String  sentence    = new String("");
+        Scanner scanner     = new Scanner(System.in);
+
+
+        System.out.println("Veuillez entrer une phrases se terminant par un point \".\" : ");
+        sentence = scanner.nextLine();
+
+        // We check if user has enter anything
+
+        // We check if sentence terminate with point
+
+        // We transform the sentence in array for seraching char
+
+        // Ask user a letter to search it to the sentence
+
+        // We check if user has enter anything
+        // We check if user has enter a letter
+
+        // Transform the letter to a char
+
+
+        // If there is a point at the first char, show empty sentence
+
+
+        // Else,
+            // Iterate to all letters in sentence
+
+                // If there is the letter in this index, increment result
+
+            // If there is a result, show it and show how many
+
+            // Else, show there is not result
+
+        scanner.close();
     }
 }
