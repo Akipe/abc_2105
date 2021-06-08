0cf277d07392a187a134544496e356bd10d9460a
Julien Milletre Akipe (asdHome) <code.julien@milletre.fr>
2021-06-08 09:22:41 +0200

    Add initial exercice 3.2 search letter in sentence

---
+++ java/src/searchletterinsentence/App.java
@@ -0,0 +1,16 @@
+package searchletterinsentence;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Exercice 3.2: Recherche une lettre dans une chaine de caractère.");
+
+        /**
+         * 
+         * Open the file exo_3.2_recherche_lettre_chaine_caracteres.png
+         * or exo_3.2_recherche_lettre_chaine_caracteres.eap with Enterprise Architect 7.1
+         */
+
+
+        
+    }
+}
