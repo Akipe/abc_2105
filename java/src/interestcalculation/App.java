050a2e86d2cce6729fd51bbe49f30e63deec4c62
Julien Milletre Akipe (aslBigRhino) <code.julien@milletre.fr>
2021-06-15 17:15:59 +0200

    Exercice 1.5 refactor with functions & add user input check

--- java/src/interestcalculation/App.java
+++ java/src/interestcalculation/App.java
@@ -3,6 +3,8 @@ package interestcalculation;
 import java.util.Scanner;
 
 public class App {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Exercice 1.5 : Calcul d'intérêts.");
 
@@ -32,32 +34,74 @@ public class App {
         FIN_ALGORITHME
          */
 
+        int     numberYearsPlacementN   = 0;
         double  initialSumS             = 0,
-                interestOfferedI        = 0,
-                numberYearsPlacementN   = 0,
-                simpleInterest          = 0,
-                compoundInterest        = 0;
-        Scanner scanner                 = new Scanner(System.in);
+                interestOfferedI        = 0;
 
 
-        System.out.println("Veuillez entrer la somme initial sur votre compte en banque :");
-        initialSumS = scanner.nextFloat();
+        initialSumS             = askUserNumberDouble("Veuillez entrer la somme initial sur votre compte en banque :");
+        interestOfferedI        = askUserNumberDouble("Veuillez entrer l'intérêt offert par la banque :");
+        numberYearsPlacementN   = askUserNumberYears();
 
-        System.out.println("Veuillez entrer l'intérêt offert par la banque :");
-        interestOfferedI = scanner.nextFloat();
+        System.out.println(
+            "Voici le résultat suite à l'intérêt simple : " + calculSimpleInterest(initialSumS, interestOfferedI, numberYearsPlacementN)
+        );
+        System.out.println(
+            "Voici le résultat suite à l'intérêt composé : " + calculationCompoundInterest(initialSumS, interestOfferedI, numberYearsPlacementN)
+        );
+    }
 
-        System.out.println("Veuillez entrer le nombre d'années pendant lequel vous laissez fructifier votre argent :");
-        numberYearsPlacementN = scanner.nextFloat();
+    private static double askUserNumberDouble(String askMessage)
+    {
+        String userInput    = "";
+        double number       = 0;
 
-        // Calculation of simple interest (interestOfferedI/100 because it is a pourcent)
-        simpleInterest      = initialSumS * (1 + numberYearsPlacementN * (interestOfferedI/100));
+        do {
+            try {
+                System.out.println(askMessage);
+                userInput = scanner.nextLine();
+                number = Double.parseDouble(userInput);
 
-        // Calculation of compound interest
-        compoundInterest    = initialSumS * (Math.pow((1 + (interestOfferedI / 100)), numberYearsPlacementN));
+                if (number <= 0) {
+                    throw new Exception("Erreur: veuillez entrer un nombre positif supérieur à 0.");
+                }
 
-        System.out.println("Voici le résultat suite à l'intérêt simple : " + simpleInterest);
-        System.out.println("Voici le résultat suite à l'intérêt composé : " + compoundInterest);
+                return number;
+            } catch (Exception error) {
+                System.err.println("Erreur : veuillez entrer un nombre positif supérieur à 0.");
+            }
+        } while (true);
+    }
 
-        scanner.close();
+    private static int askUserNumberYears()
+    {
+        String  userInput   = "";
+        int     years       = 0;
+
+        do {
+            try {
+                System.out.println("Veuillez entrer le nombre d'années pendant lequel vous laissez fructifier votre argent :");
+                userInput = scanner.nextLine();
+                years = Integer.parseInt(userInput);
+
+                if (years <= 0) {
+                    throw new Exception("Erreur : veuillez entrer un nombre d'années positif et supérieur à 0.");
+                }
+
+                return years;
+            } catch (Exception error) {
+                System.err.println("Erreur : veuillez entrer au moin 1 année ou plus.");
+            }
+        } while (true);
+    }
+
+    private static double calculSimpleInterest(double initialSumS, double interestOfferedI, int numberYearsPlacementN)
+    {
+        return (initialSumS * (1 + (double)numberYearsPlacementN * (interestOfferedI/100)));
+    }
+
+    private static double calculationCompoundInterest(double initialSumS, double interestOfferedI, int numberYearsPlacementN)
+    {
+        return (initialSumS * (Math.pow((1 + (interestOfferedI / 100)), (double)numberYearsPlacementN)));
     }
 }
