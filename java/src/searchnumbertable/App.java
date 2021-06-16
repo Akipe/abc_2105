9b549da3534a656a679f1deb8d5dc35c38fe7f43
Julien Milletre Akipe (aslBigRhino) <code.julien@milletre.fr>
2021-06-16 16:36:36 +0200

    Exercice 3.1 refactor code
    - seperate code in functions
    - fix  errors when checking if number is in list

--- java/src/searchnumbertable/App.java
+++ java/src/searchnumbertable/App.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class App {
+    private static Scanner  scanner                 = new Scanner(System.in);
+    private static int[]    listOfCrescentNumbers   = { 0 };
+
     public static void main(String[] args) {
         System.out.println("Exercice 3.1: Recherche un nombre dans un tableau.");
 
@@ -13,15 +16,14 @@ public class App {
          * or exo_3.1_recherche_nombre_tableau.eap with Enterprise Architect 7.1
          */
 
-         int        numberToAddToList           = 0,
-                    numberUsedToSearchInList    = 0,
-                    index                       = 0;
-         Boolean    userWantAddNumbersToTheList = true,
-                    userWantSearchNumber        = true,
-                    isNumberFindInList          = false;
-         String     userCommand                 = new String("");
-         Scanner    scanner                     = new Scanner(System.in);
-         int[]      listOfCrescentNumbers       = { 0 };
+        askUserToPrepareCrescentNumberList();
+        askUserToSearchNumberInCrescentNumberListOrExit();
+    }
+
+    private static void askUserToPrepareCrescentNumberList()
+    {
+        String  userCommand                 = "";
+        Boolean userWantAddNumbersToTheList = true;
 
         do {
             System.out.println("Veuillez entrer un nombre à ajouter dans la liste,");
@@ -34,39 +36,83 @@ public class App {
                     System.out.println("");
                     break;
                 case "clear":
-                    listOfCrescentNumbers = new int[] { 0 };
-                    System.out.println("Suppression des nombres sauvegardés...\n");
+                    clearListOfCrescentNumbers();
                     break;
                 case "show":
-                    System.out.println("\nVoici l'ensemble des nombres de votre liste :");
-                    for (index = 0; index < listOfCrescentNumbers.length; index++) {
-                        System.out.println("Nombre n°" + (index + 1) + ": " + listOfCrescentNumbers[index]);
-                    }
-                    System.out.println("");
+                    showListCrescentNumbersSave(listOfCrescentNumbers);
                     break;
                 default:
-                    try {
-                        numberToAddToList = Integer.parseInt(userCommand);
-                        
-                        // Check if the number is bigger than the previous (for crescent numbers)
-                        if (listOfCrescentNumbers[listOfCrescentNumbers.length - 1] <= numberToAddToList) {
-                            if (!(listOfCrescentNumbers.length == 1 && listOfCrescentNumbers[0] == 0)) { // Check if we have a not empty list
-                                listOfCrescentNumbers = Arrays.copyOf(listOfCrescentNumbers, listOfCrescentNumbers.length + 1);
-                            }
-
-                            listOfCrescentNumbers[listOfCrescentNumbers.length - 1] = numberToAddToList;
-
-                            System.out.println("");
-                        } else {
-                            System.err.println("Erreur : Veuillez entrer un nombre égale ou plus grand que " + listOfCrescentNumbers[listOfCrescentNumbers.length - 1] + "\n");
-                        }
-                        
-                    } catch(NumberFormatException e) { // We check if user has enter a number
-                        System.err.println("Erreur : Veuillez entrer uniquement un nombre, sans espace ni d'autres caractères.\n");
-                    }
+                    askUserAddCrescentNumberToList(userCommand);
             }
         } while (userWantAddNumbersToTheList);
-        
+    }
+
+    private static void clearListOfCrescentNumbers()
+    {
+        listOfCrescentNumbers = new int[] { 0 };
+        System.out.println("Suppression des nombres sauvegardés...\n");
+    }
+
+    private static void showListCrescentNumbersSave(int[] listOfCrescentNumbers)
+    {
+        int index = 0;
+
+        System.out.println("\nVoici l'ensemble des nombres de votre liste :");
+
+        for (index = 0; index < listOfCrescentNumbers.length; index++) {
+            System.out.println("Nombre n°" + (index + 1) + ": " + listOfCrescentNumbers[index]);
+        }
+        System.out.println("");
+    }
+
+    private static void askUserAddCrescentNumberToList(String userCommand)
+    {
+        int numberToAddToList = 0;
+
+        try {
+            numberToAddToList = Integer.parseInt(userCommand);
+            
+            if (isSameOrBiggerThanLastNumberCrescentList(numberToAddToList)) {
+                if (isCrescentListIsNotEmpty()) {
+                    createBiggerCrescentList();
+                }
+
+                addNumberCrescentList(numberToAddToList);
+
+                System.out.println("");
+            } else {
+                System.err.println("Erreur : Veuillez entrer un nombre égale ou plus grand que " + listOfCrescentNumbers[listOfCrescentNumbers.length - 1] + "\n");
+            }
+            
+        } catch(NumberFormatException e) { // We check if user has enter a number
+            System.err.println("Erreur : Veuillez entrer uniquement un nombre, sans espace ni d'autres caractères.\n");
+        }
+    }
+
+    private static Boolean isSameOrBiggerThanLastNumberCrescentList(int numberToAddToList)
+    {
+        return listOfCrescentNumbers[listOfCrescentNumbers.length - 1] <= numberToAddToList;
+    }
+    
+    private static Boolean isCrescentListIsNotEmpty()
+    {
+        return !(listOfCrescentNumbers.length == 1 && listOfCrescentNumbers[0] == 0);
+    }
+
+    private static void createBiggerCrescentList()
+    {
+        listOfCrescentNumbers = Arrays.copyOf(listOfCrescentNumbers, listOfCrescentNumbers.length + 1);
+    }
+
+    private static void addNumberCrescentList(int numberToAddToList)
+    {
+        listOfCrescentNumbers[listOfCrescentNumbers.length - 1] = numberToAddToList;
+    }
+
+    private static void askUserToSearchNumberInCrescentNumberListOrExit()
+    {
+        String userCommand = new String("");
+
         do {
             System.out.println("Entrez un nombre à rechercher dans le tableau,");
             System.out.println("Ou \"quit\" pour arréter l'application :");
@@ -74,39 +120,53 @@ public class App {
 
             switch (userCommand) {
                 case "quit":
-                    userWantSearchNumber = false;
                     System.out.println("Au revoir :)\n");
+                    System.exit(0); // Terminate the app
                     break;
                 default:
-                    try {
-                        numberUsedToSearchInList = Integer.parseInt(userCommand);
-
-                        // Iteration of tab
-                        if (listOfCrescentNumbers[listOfCrescentNumbers.length - 1] >= numberUsedToSearchInList) {
-                            for (index = 0; numberUsedToSearchInList > listOfCrescentNumbers[index] || index < listOfCrescentNumbers.length - 1; index++) {
-                                if (numberUsedToSearchInList == listOfCrescentNumbers[index]) {
-                                    isNumberFindInList = true; // On a trouvé le nombre !!!
-                                }
-                            }
-
-                            if (isNumberFindInList) {
-                                System.out.println("Le nombre recherché est bien dans la liste !\n");
-                                
-                                // Reinitialize for next search
-                                isNumberFindInList = false;
-                            } else {
-                                System.out.println("Le nombre recherché n'est pas dans la liste, dommage.\n");
-                            }
-                        } else {
-                            System.out.println("Votre nombre est trop grand par rapport au dernier nombre croissant du tableau.\n");
-                        }
-                        
-                    } catch (NumberFormatException e) { // We check if user has enter a number
-                        System.err.println("Erreur : Veuillez entrer uniquement un nombre.\n");
-                    }
+                    tryToFindNumberInCrescentNumberList(userCommand);
             }
-        } while (userWantSearchNumber);
+        } while (true);
+    }
 
-        scanner.close();
+    private static void tryToFindNumberInCrescentNumberList(String userCommand)
+    {
+        int numberUsedToSearchInList = 0;
+
+        try {
+            numberUsedToSearchInList = Integer.parseInt(userCommand);
+
+            // Iteration of tab
+            if (isSmallerThanLastCrescentNumberList(numberUsedToSearchInList)) {
+                if (isNumberFindInCrescentNumberList(numberUsedToSearchInList)) {
+                    System.out.println("Le nombre recherché est bien dans la liste !\n");
+                } else {
+                    System.out.println("Le nombre recherché n'est pas dans la liste, dommage.\n");
+                }
+            } else {
+                System.out.println("Votre nombre est trop grand par rapport au dernier nombre croissant du tableau.\n");
+            }
+            
+        } catch (NumberFormatException e) { // We check if user has enter a number
+            System.err.println("Erreur : Veuillez entrer uniquement un nombre ou \"quit\".\n");
+        }
+    }
+
+    private static Boolean isSmallerThanLastCrescentNumberList(int number)
+    {
+        return listOfCrescentNumbers[listOfCrescentNumbers.length - 1] >= number;
+    }
+
+    private static Boolean isNumberFindInCrescentNumberList(int number)
+    {
+        int index = 0;
+
+        for (index = 0; index < listOfCrescentNumbers.length; index++) {
+            if (number == listOfCrescentNumbers[index]) {
+                return true; // We find the number
+            }
+        }
+
+        return false;
     }
 }
