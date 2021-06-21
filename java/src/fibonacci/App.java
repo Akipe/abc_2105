571366a26ec41860dfb7380c5f187949b5a55d4e
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-06-21 08:12:37 +0200

    fibonacci simplify calcul for first month

--- java/src/fibonacci/App.java
+++ java/src/fibonacci/App.java
@@ -6,7 +6,7 @@ public class App {
     private static long newTotalRabbitsCouple;
 
     public static void main(String[] args) {
-        for (int howManyIterator = 1; howManyIterator < 100; howManyIterator++) {
+        for (int howManyIterator = 1; howManyIterator < 20; howManyIterator++) {
             calcFibonacci(howManyIterator);
             showResult(howManyIterator);
         }
@@ -18,12 +18,10 @@ public class App {
         adultsRabbitsCouple     = 0;
         newTotalRabbitsCouple   = 0;
 
-        for (int month = 0; month < howManyMonth; month++) {
-            if (month != 0) {
-                newTotalRabbitsCouple   = allCurrentRabbitCouple + adultsRabbitsCouple;
-                adultsRabbitsCouple     = allCurrentRabbitCouple;
-                allCurrentRabbitCouple  = newTotalRabbitsCouple;
-            }
+        for (int month = 1; month < howManyMonth; month++) {
+            newTotalRabbitsCouple   = allCurrentRabbitCouple + adultsRabbitsCouple;
+            adultsRabbitsCouple     = allCurrentRabbitCouple;
+            allCurrentRabbitCouple  = newTotalRabbitsCouple;
         }
     }
 
