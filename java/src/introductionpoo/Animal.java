d41eae91e3372b6827b4ce243ff796befc72c3f5
Julien Milletre Akipe (aslBigRhino) <code.julien@milletre.fr>
2021-06-23 12:35:16 +0200

    Java & POO add introduction

---
+++ java/src/introductionpoo/Animal.java
@@ -0,0 +1,24 @@
+package introductionpoo;
+
+public class Animal {
+    private String specie;
+
+    public Animal()
+    {
+        System.out.println("Constructeur de Animal.");
+
+        this.specie = "Cheval";
+    }
+
+    public  String getSpecie()
+    {
+        return this.specie;
+    }
+
+    public void setSpecie(String _toto)
+    {
+        if  (_toto.length() > 2) {
+            this.specie = _toto;
+        }
+    }
+}
---
+++ java/src/introductionpoo/App.java
@@ -0,0 +1,17 @@
+package introductionpoo;
+
+public class App {
+    public static void main(String[] args) {
+        String animalSpecie;
+
+        Animal bee = new Animal();
+
+        animalSpecie = bee.getSpecie();
+        System.out.println(animalSpecie);
+
+        bee.setSpecie("Abeille");
+
+        animalSpecie = bee.getSpecie();
+        System.out.println(animalSpecie);
+    }
+}
