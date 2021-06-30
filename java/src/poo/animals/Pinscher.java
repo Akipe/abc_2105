52d6230348eaa73ffd8fb300f46e29bdd8bec4b4
Julien Milletre Akipe (aslBigRhino) <code.julien@milletre.fr>
2021-06-30 14:58:57 +0200

    POO animal add pinscher class

--- java/src/poo/animals/Pinscher.java
+++ java/src/poo/animals/Pinscher.java
@@ -6,17 +6,44 @@ package poo.animals;
  * @created 29-juin-2021 12:43:36
  */
 public class Pinscher extends Dog {
+    public static final int WAIT_MILISECONDS_BEFORE_BARK = 10000;
+    private long lastBarkTimeMiliseconds;
 
     /**
      * 
      * @param _name
      */
-    public Pinscher(String _name){
+    public Pinscher(String _name)
+    {
         super(_name);
     }
 
-    public Dog bark(){
-        return null;
+    public Dog bark()
+    {
+        long currentTimeMiliseconds = getCurrentTime();
+
+        if (hasWaitEnoughBeforeBark(currentTimeMiliseconds)) {
+            System.out.println("Woaf woaf !!!");
+            setLastBarkTime(currentTimeMiliseconds);
+        } else {
+            System.out.println("...");
+        }
+
+        return this;
     }
 
+    private Boolean hasWaitEnoughBeforeBark(long _currentTimeMiliseconds)
+    { 
+        return ((_currentTimeMiliseconds - this.lastBarkTimeMiliseconds) >= (long) WAIT_MILISECONDS_BEFORE_BARK);
+    }
+
+    private void setLastBarkTime(long _currentTimeMiliseconds)
+    {
+        this.lastBarkTimeMiliseconds = _currentTimeMiliseconds;
+    }
+
+    private long getCurrentTime()
+    {
+        return System.currentTimeMillis();
+    }
 }
