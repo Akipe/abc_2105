13752a5773a415a4dab8b8ef97f9bce97303266b
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-06-30 10:37:19 +0200

    POO animal add dog class

--- java/src/poo/animals/Dog.java
+++ java/src/poo/animals/Dog.java
@@ -6,29 +6,40 @@ package poo.animals;
  * @created 29-juin-2021 12:43:31
  */
 public class Dog extends Animal {
-
-    public Dog(){
-
-    }
+    private String name;
 
     /**
      * 
-     * @param name
+     * @param _name
      */
-    public Dog Dog(String name){
-        return null;
+    public Dog(String _name){
+        super("chien");
+
+        try {
+            if (!_name.isEmpty()) {
+                this.name = _name;
+            } else {
+                throw new Exception("Veuillez entrer un nom pour votre chient.");
+            }
+        } catch (Exception error) {
+            System.err.println("Erreur : " + error.getMessage());
+        }
     }
 
     public String getName(){
-        return "";
+        return this.name;
     }
 
     public Dog feed(){
-        return null;
+        System.out.println("Le " + this.getSpecies() + " " + this.getName() + " est entrain de manger sa pâtée.");
+
+        return this;
     }
 
     public Dog bark(){
-        return null;
+        System.out.println("Le " + this.getSpecies() + " " + this.getName() + " est entrain d'aboyer : woaf woaf!");
+
+        return this;
     }
 
 }
