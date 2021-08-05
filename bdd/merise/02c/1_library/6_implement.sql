e9f73ad794659051dc6c5c37dc61c57e9a025e19
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-08-05 16:39:46 +0200

    BDD library add first version sql script

--- bdd/merise/02c/1_library/6_implement.sql
+++ bdd/merise/02c/1_library/6_implement.sql
@@ -1,10 +1,12 @@
-DROP DATABASE IF EXISTS library;
+DROP DATABASE IF EXISTS library_db;
 
-CREATE DATABASE IF NOT EXISTS library;
+CREATE DATABASE IF NOT EXISTS library_db;
+
+USE library_db;
 
 CREATE TABLE customers
 (
-    customers_id INT PRIMARY KEY,
+    customers_id INT PRIMARY KEY AUTO_INCREMENT,
     customers_first_name VARCHAR(64) NOT NULL,
     customers_last_name VARCHAR(64) NOT NULL,
     customers_address VARCHAR(64) NOT NULL,
@@ -12,10 +14,28 @@ CREATE TABLE customers
 );
 
 CREATE TABLE books
-();
-
-CREATE TABLE write
-();
+(
+    books_id INT PRIMARY KEY,
+    books_title VARCHAR(256) NOT NULL,
+    books_editor VARCHAR(256) NOT NULL,
+    books_date_purchase DATE NOT NULL,
+    books_borrow_date DATE NULL,
+    books_condition VARCHAR(512),
+    customers_id INT,
+    FOREIGN KEY (customers_id) REFERENCES customers(customers_id)
+);
 
 CREATE TABLE writers
-();
+(
+    writers_id INT PRIMARY KEY,
+    writers_first_name VARCHAR(64) NOT NULL,
+    writers_last_name VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE write_t
+(
+    books_id INT NOT NULL,
+    writers_id INT NOT NULL,
+    FOREIGN KEY (books_id) REFERENCES books(books_id),
+    FOREIGN KEY (writers_id) REFERENCES writers(writers_id)
+);
