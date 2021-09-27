4fdd024dace5895e2883b3b1ddd9a55f272d976e
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-09-27 12:50:16 +0200

    JS add first exercice

---
+++ web/javascript/src/ex1/app.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 1
+    let value1 = "Toto";
+    let value2 = "Tata";
+
+    console.log(`Valeur des variables :`)
+    console.log(`value1 = ${value1}`)
+    console.log(`value2 = ${value2}`)
+
+    value1 = value1 + "_" + value2;
+    value2 = value1;
+    value1 = value1.split('_')[1];
+    value2 = value2.split('_')[0];
+    
+    console.log(`Valeur des variables :`)
+    console.log(`value1 = ${value1}`)
+    console.log(`value2 = ${value2}`)
+
+
+    // 2
+    let firstname   = "";
+    let lastname    = "";
+    let age         = 0;
+
+    firstname = prompt("Entrez votre prénom");
+    lastname = prompt("Entrez votre nom");
+    age = prompt("Entrez votre age");
+
+    console.log(`Votre prénom est ${firstname}, votre nom est ${lastname} et vous avez ${age}.`);
+
+
+    // 3
+    let yearBirthDay = 0;
+    let currentYear = 0;
+    age = 0;
+
+    yearBirthDay = prompt("Entrez votre année de naissance");
+
+    currentYear = new Date().getFullYear();
+    age = currentYear - yearBirthDay;
+
+    console.log(`Vous avez ${age} ans.`);
+
+
+    // 4
+    const taxeInPercent = 20.0;
+    let priceDutyFree = 0.0;
+    let priceWithTaxe = 0.0;
+
+    priceDutyFree = parseFloat(prompt("Entrez le prix hors taxe"));
+    priceWithTaxe = ((priceDutyFree / 100.0) * 20) + priceDutyFree;
+
+    console.log(`Le prix TTC de ${priceDutyFree}€ est de ${priceWithTaxe}€`);
+
+
+    // 5
+    console.log("Écrire de plusieurs façons différentes une même phrase. (1)");
+
+    let sentence = "Écrire de plusieurs façons différentes une même phrase.";
+    console.log(sentence + " (2)");
+
+    console.log("Écrire" + " " + "de plusieurs façons" + " différentes " + "une même phrase. (3)")
+
+    let sentenceArray = ["Écrire", "de plusieurs façons", "différentes", "une même phrase."];
+    console.log(sentenceArray.join(" ") + " (4)");
+
+    let completeSentence = "";
+    for (let index = 0; index < sentenceArray.length; index++) {
+        completeSentence += sentenceArray[index] + " ";
+    }
+    console.log(completeSentence + "(5)");
+});
