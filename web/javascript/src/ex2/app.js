8b27535f7847e36294e95cc4683af0c01656bf95
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-09-28 12:33:38 +0200

    JS ex2 ad n°1, 2, 3

--- web/javascript/src/ex2/app.js
+++ web/javascript/src/ex2/app.js
@@ -18,34 +18,100 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     // 2
-    let nom1 = "";
-    let nom2 = "";
-    let nom3 = "";
+    let name1 = "";
+    let name2 = "";
+    let name3 = "";
     let isAlphabeticalOrder = true;
+    let stayCharChecking = true;
     let charNumber = 0;
-    let nodeText = document.getElementById("Text");
 
-    console.log(nodeText);
+    name1 = prompt("Veuillez entrer votre premier nom");
+    name2 = prompt("Veuillez entrer le deuxième nom");
+    name3 = prompt("Veuillez entrer le troisième nom");
 
-    nom1 = prompt("Veuillez entrer votre premier nom");
-    nom2 = prompt("Veuillez entrer le deuxième nom");
-    nom3 = prompt("Veuillez entrer le troisième nom");
+    if (isWordsAreAlphabeticOrder(name1, name2, name3)) {
+        console.log("Les nom sont dans l'ordre alphabétique.");
+    } else {
+        console.log("Les nom ne sont pas dans l'ordre alphabétique.")
+    }
 
 
-    charNumber = 0;
-    // do {
-        if (nom1[charNumber] <= nom2[charNumber] && nom2[charNumber] <= nom3[charNumber]) {
-            nodeText.textContent = "Les noms sont dans l'ordre alphabétique.";
-        } else {
-            if (nom1[charNumber] <= nom2[charNumber]) {
-                nodeText.textContent = "Les deux premiers nom sont dans l'ordre croissant.";
-            } else if (nom2[charNumber] <= nom3[charNumber]) {
-                nodeText.textContent = "Les deux derniers nom sont dans l'ordre croissant.";
-            }
-            nodeText.textContent = "Les noms ne sont pas dans tous dans l'ordre croissant";
-        }
+    // 3
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let time;
 
-    //     charNumber++;
-    // } while (isAlphabeticalOrder);
+    hours = parseInt(prompt("Veuillez entrer un nombre d'heures"));
+    minutes = parseInt(prompt("Veuillez entrer un nombre de minutes"));
+    seconds = parseInt(prompt("Veuillez entrer un nombre de secondes"));
+    time = fixTime(hours, minutes, seconds);
+    
+    console.log(`Le temps rentré par l'utilisateur est : ${time.hours} heures, ${time.minutes} minutes et ${time.seconds} seconds.`);
 
+    console.log("On ajout 1 seconde...")
+    seconds++;
+    time = fixTime(hours, minutes, seconds);
+    console.log(`Le temps rentré par l'utilisateur avec l'ajout d'une seconde est : ${time.hours} heures, ${time.minutes} minutes et ${time.seconds} seconds.`);
 });
+
+
+// 2
+function isWordsAreAlphabeticOrder(_word1, _word2, _word3) {
+    if (isWordIsBeforeOrEqual(_word1, _word2)) {
+        if (isWordIsBeforeOrEqual(_word2, _word3)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// 2
+function isWordIsBeforeOrEqual(_word1, _word2) {
+    if (_word1.localeCompare(_word2, 'en', { sensitivity: 'base' }) <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 3
+function fixTime(_hours, _minutes, _seconds) {
+    let timeCalc;
+
+    timeCalc = fixSeconds(_seconds);
+    _seconds = timeCalc.seconds;
+    _minutes = _minutes + timeCalc.minutes;
+
+    timeCalc = fixMinutes(_minutes);
+    _minutes = timeCalc.minutes;
+    _hours = _hours + timeCalc.hours;
+
+    return {
+        'hours': _hours,
+        'minutes': _minutes,
+        'seconds': _seconds
+    }
+}
+
+function fixMinutes(_minutes) {
+    return fixUnitTime(_minutes, "minutes", 60, "hours");
+}
+
+function fixSeconds(_seconds) {
+    return fixUnitTime(_seconds, "seconds", 60, "minutes");
+}
+
+function fixUnitTime(_quantitiesUnitTime, _nameUnitTime, _maxUnitTime, _nameSuperiorUnitTime) {
+    let superiorUnitTime = 0;
+
+    if (_quantitiesUnitTime >= _maxUnitTime) {
+        superiorUnitTime = Math.floor(_quantitiesUnitTime / _maxUnitTime);
+        _quantitiesUnitTime = _quantitiesUnitTime % 60;
+    }
+
+    return {
+        [_nameUnitTime]: _quantitiesUnitTime,
+        [_nameSuperiorUnitTime]: superiorUnitTime
+    }
+}
