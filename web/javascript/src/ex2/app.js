d22ef8611c17da6740fc98f1ca234519fc89a422
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-09-29 15:11:05 +0200

    JS ex2 add n°5

--- web/javascript/src/ex2/app.js
+++ web/javascript/src/ex2/app.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 1
     exercice1();
 
-    // 2
     exercice2();
 
-    // 3
     exercice3();
 
-    // 4
     exercice4();
+
+    exercice5();
 });
 
 // 1
@@ -70,25 +68,27 @@ function isWordIsBeforeOrEqual(_word1, _word2) {
 
 // 3
 function exercice3() {
+    let days = 0;
     let hours = 0;
     let minutes = 0;
     let seconds = 0;
     let time;
 
+    days = parseInt(prompt("Veuillez entrer un nombre de jours"));
     hours = parseInt(prompt("Veuillez entrer un nombre d'heures"));
     minutes = parseInt(prompt("Veuillez entrer un nombre de minutes"));
     seconds = parseInt(prompt("Veuillez entrer un nombre de secondes"));
-    time = fixTime(hours, minutes, seconds);
+    time = fixTime(days, hours, minutes, seconds);
     
-    console.log(`Le temps rentré par l'utilisateur est : ${time.hours} heures, ${time.minutes} minutes et ${time.seconds} seconds.`);
+    console.log(`Le temps rentré par l'utilisateur est : ${time.days} jours, ${time.hours} heures, ${time.minutes} minutes et ${time.seconds} seconds.`);
 
     console.log("On ajout 1 seconde...")
     seconds++;
-    time = fixTime(hours, minutes, seconds);
-    console.log(`Le temps rentré par l'utilisateur avec l'ajout d'une seconde est : ${time.hours} heures, ${time.minutes} minutes et ${time.seconds} seconds.`);
+    time = fixTime(days, hours, minutes, seconds);
+    console.log(`Le temps rentré par l'utilisateur avec l'ajout d'une seconde est : ${time.days} jours, ${time.hours} heures, ${time.minutes} minutes et ${time.seconds} seconds.`);
 }
 
-function fixTime(_hours, _minutes, _seconds) {
+function fixTime(_days, _hours, _minutes, _seconds) {
     let timeCalc;
 
     timeCalc = fixSeconds(_seconds);
@@ -99,11 +99,20 @@ function fixTime(_hours, _minutes, _seconds) {
     _minutes = timeCalc.minutes;
     _hours = _hours + timeCalc.hours;
 
+    timeCalc = fixHours(_hours);
+    _hours = timeCalc.hours;
+    _days = _days + timeCalc.days;
+
     return {
+        'days': _days,
         'hours': _hours,
         'minutes': _minutes,
         'seconds': _seconds
     }
+}
+
+function fixHours(_hours) {
+    return fixUnitTime(_hours, "hours", 24, "days");
 }
 
 function fixMinutes(_minutes) {
@@ -119,12 +128,40 @@ function fixUnitTime(_quantitiesUnitTime, _nameUnitTime, _maxUnitTime, _nameSupe
 
     if (_quantitiesUnitTime >= _maxUnitTime) {
         superiorUnitTime = Math.floor(_quantitiesUnitTime / _maxUnitTime);
-        _quantitiesUnitTime = _quantitiesUnitTime % 60;
+        _quantitiesUnitTime = _quantitiesUnitTime % _maxUnitTime;
     }
 
     return {
         [_nameUnitTime]: _quantitiesUnitTime,
         [_nameSuperiorUnitTime]: superiorUnitTime
+    }
+}
+
+function fixMinute(_minutes) {
+    let hours = 0;
+
+    if (_minutes >= 60) {
+        hours = Math.floor(_minutes / 60);
+        _minutes = _minutes % 60;
+    }
+
+    return {
+        "minutes": _minutes,
+        "hours": hours
+    }
+}
+
+function fixMinute(_minutes) {
+    let hours = 0;
+
+    if (_minutes >= 60) {
+        hours = Math.floor(_minutes / 60);
+        _minutes = _minutes % 60;
+    }
+
+    return {
+        "minutes": _minutes,
+        "hours": hours
     }
 }
 
@@ -147,4 +184,75 @@ function forceAskNumberBetween1900And2021() {
     } while (!isNumberIsBetween1900And2021(userNumber));
 
     return userNumber;
+}
+
+// 5
+function exercice5() {
+    let userNumber = 0;
+    let minNumberRandom = 0;
+    let maxNumberRandom = 0;
+    let numberToFind = 0;
+    let askUserInterval;
+
+    askUserInterval = askUserIntervalForRandomNumber();
+
+    minNumberRandom = parseInt(askUserInterval['minNumberRandom']);
+    maxNumberRandom = parseInt(askUserInterval['maxNumberRandom']);
+
+    numberToFind = getRandomNumberFrom(minNumberRandom, maxNumberRandom);
+
+    do {
+        do {
+            userNumber = askUserNumber(minNumberRandom, maxNumberRandom);
+
+            if (userNumber < minNumberRandom || userNumber > maxNumberRandom) {
+                console.log(`Le nombre ${userNumber} ne fait pas partie de l'interval entre ${minNumberRandom} et ${maxNumberRandom}.`)
+            }
+        } while (userNumber < minNumberRandom || userNumber > maxNumberRandom)
+
+        if (userNumber == numberToFind) {
+            console.log("Bien joué ! Vous avez trouvé le nombre !");
+        } else if (userNumber > numberToFind) {
+            console.log(`Le nombre mystère est inférieur à ${userNumber}`);
+        } else {
+            console.log(`Le nombre mystère est supérieur à ${userNumber}`);
+        }
+    } while(userNumber != numberToFind);
+}
+
+function askUserIntervalForRandomNumber() {
+    let isIntervalIsNotValid = false;
+
+    do {
+        minNumberRandom = parseInt(prompt("Entrez le nombre le plus petit pour définir l'interval du nombre aléatoire :"));
+        maxNumberRandom = parseInt(prompt("Entrez le nombre le plus grand pour définir l'interval du nombre aléatoire :"));
+
+        if (minNumberRandom > maxNumberRandom) {
+            console.log("Le nombre le plus petit ne peut être supérieur au nombre le plus grand!");
+        } else if (minNumberRandom == maxNumberRandom) {
+            console.log("Les deux nombres ne peuvent être égales!");
+        } else {
+            console.log("Interval valide !");
+        }
+
+    } while (minNumberRandom >= maxNumberRandom);
+
+    return {
+        'minNumberRandom': minNumberRandom,
+        'maxNumberRandom': maxNumberRandom
+    };
+}
+
+function getRandomNumberFrom(_minNumberRandom, _maxNumberRandom) {
+    return Math.floor(Math.random() * (_maxNumberRandom - _minNumberRandom + 1)) + _minNumberRandom;
+}
+
+function askUserNumber(_minNumber, _maxNumber) {
+    let userInput = 0;
+
+    do {
+        userInput = prompt(`Veuillez entrer un nombre entre ${_minNumber} et ${_maxNumber} :`);
+    } while (Number.isInteger(userInput));
+    
+    return parseInt(userInput);
 }
