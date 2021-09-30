1956cc0c4019b233bc5b8ae893b8bf5612b4271c
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-09-30 12:43:02 +0200

    JS ex4 n°1 add function for get lower number and ascending order

--- web/javascript/src/ex4/app.js
+++ web/javascript/src/ex4/app.js
@@ -1,6 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
+    exercice1();
+
     exercice3();
 });
+
+// 1
+
+function exercice1() {
+    let userArray = Array();
+
+    userArray = promptTab();
+
+    console.log(`Le nombre le plus petit est ${getLowerNumberInArray(userArray)}`);
+    console.log(`${arrayInAscending(userArray)}`);
+}
+
+function getLowerNumberInArray(_array) {
+    let currentLowerNumber;
+
+    for (let index = 0; index < _array.length; index++) {
+        if (!Number.isNaN(Number.parseFloat(_array[index]))) {
+            if (typeof currentLowerNumber === 'undefined') {
+                currentLowerNumber = Number.parseFloat(_array[index]);
+            } else {
+                if (Number.parseFloat(currentLowerNumber) > Number.parseFloat(_array[index])) {
+                    currentLowerNumber = _array[index];
+                }
+            }
+        }
+    }
+
+    return currentLowerNumber;
+}
+
+function arrayInAscending(_array) {
+    let temp = 0;
+    let needReCheck = false;
+
+    for (let index = 0; index < _array.length; index++) {
+        if (parseFloat(_array[index]) > parseFloat(_array[index + 1])) {
+            temp = _array[index];
+            _array[index] = _array[index + 1];
+            _array[index + 1] = temp;
+            needReCheck = true;
+        }
+    }
+
+    if (needReCheck) {
+        _array = arrayInAscending(_array);
+    }
+
+    return _array;
+}
 
 // 3
 function exercice3() {
@@ -40,4 +91,18 @@ function promptInt(_message) {
 
         console.log("Veuillez entrer un nombre entier !");
     } while (true);
+}
+
+function promptTab() {
+    let userInput = "";
+    let tabOfValue = [];
+
+    do {
+        userInput = prompt("Veuillez entrer une donnée à stocker dans votre tableau (ou une chaine vide pour arréter");
+        if (userInput != "") {
+            tabOfValue.push(userInput);
+        }
+    } while (userInput != "");
+
+    return tabOfValue;
 }
