e9c16e69d77a3e1d5ae4a47ecf80586ff1ea230c
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-10-05 10:59:25 +0200

    JS counter add  counter for minutes

--- web/javascript/src/ex_counter/counter.js
+++ web/javascript/src/ex_counter/counter.js
@@ -21,3 +21,21 @@ let compteSeconde = new Compteur(0, 1000);
 span.parentElement.replaceChild(compteSeconde.spanElement, span);
 
 
+console.log(compteSeconde.compte);
+
+setMinutes(".empty");
+
+function setMinutes(_querySelector) {
+    const milisecondesForOneMinute = 60000;
+    let counterMinutes;
+    let parentNode;
+    let pNode;
+
+    counterMinutes = new Compteur(0, milisecondesForOneMinute);
+    parentNode = document.querySelector(_querySelector);
+
+    parentNode.innerHTML = "<p>Temps passé en minutes = <span>0</span></p>";
+
+    pNode = parentNode.children[0];
+    pNode.replaceChild(counterMinutes.spanElement, document.querySelector(_querySelector + " span"))
+}
